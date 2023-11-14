7780c6c61902fc49b892b65ad2cf543d70f12caa
Senad Topalović <senad.topalovic@hotmail.com>
2023-11-14 02:31:09 +0100

    stage 1

--- src/Program.cs
+++ src/Program.cs
@@ -14,27 +14,27 @@ if (command == "decode")
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     Console.WriteLine("Logs from your program will appear here!");
 
-    // Uncomment this line to pass the first stage
-    //var encodedValue = param;
-    //if (Char.IsDigit(encodedValue[0]))
-    //{
-    //    // Example: "5:hello" -> "hello"
-    //    var colonIndex = encodedValue.IndexOf(':');
-    //    if (colonIndex != -1)
-    //    {
-    //        var strLength = int.Parse(encodedValue[..colonIndex]);
-    //        var strValue = encodedValue.Substring(colonIndex + 1, strLength);
-    //        Console.WriteLine(JsonSerializer.Serialize(strValue));
-    //    }
-    //    else
-    //    {
-    //        throw new InvalidOperationException("Invalid encoded value: " + encodedValue);
-    //    }
-    //}
-    //else
-    //{
-    //    throw new InvalidOperationException("Unhandled encoded value: " + encodedValue);
-    //}
+     //Uncomment this line to pass the first stage
+    var encodedValue = param;
+    if (Char.IsDigit(encodedValue[0]))
+    {
+        // Example: "5:hello" -> "hello"
+        var colonIndex = encodedValue.IndexOf(':');
+        if (colonIndex != -1)
+        {
+            var strLength = int.Parse(encodedValue[..colonIndex]);
+            var strValue = encodedValue.Substring(colonIndex + 1, strLength);
+            Console.WriteLine(JsonSerializer.Serialize(strValue));
+        }
+        else
+        {
+            throw new InvalidOperationException("Invalid encoded value: " + encodedValue);
+        }
+    }
+    else
+    {
+        throw new InvalidOperationException("Unhandled encoded value: " + encodedValue);
+    }
 }
 else
 {
