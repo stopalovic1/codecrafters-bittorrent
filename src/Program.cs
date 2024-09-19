f5a10c645353dd7e77adb949aba3bce655ba68e2
Senad Topalović <senad.topalovic@hotmail.com>
2024-09-19 04:28:28 +0200

    jah

--- src/Program.cs
+++ src/Program.cs
@@ -48,6 +48,26 @@ if (command == "decode")
         }
         Console.WriteLine(JsonSerializer.Serialize(parsedValue));
     }
+    else if (encodedValue[0] == 'l')
+    {
+        var firstIndex = encodedValue.IndexOf("l");
+        var lastIndex = encodedValue.LastIndexOf("e");
+        if (firstIndex == -1 || lastIndex == -1 || (firstIndex == -1 && lastIndex == -1))
+        {
+            throw new InvalidOperationException("Unhandled encoded value: " + encodedValue);
+        }
+        var strLength = lastIndex - firstIndex;
+        var listString = encodedValue.Substring(firstIndex + 1, strLength - 1);
+        var splitArray = listString.Split(':');
+        var numberString = int.Parse(splitArray[0]);
+        var stringPart = splitArray[1].Substring(0, numberString);
+
+        var numberPart = splitArray[1].Substring(numberString);
+        var f = DecodeNumber(numberPart);
+        object[] array = { stringPart, f };
+        string serialized = JsonSerializer.Serialize(array);
+        Console.WriteLine(serialized);
+    }
     else
     {
         throw new InvalidOperationException("Unhandled encoded value: " + encodedValue);
@@ -56,4 +76,37 @@ if (command == "decode")
 else
 {
     throw new InvalidOperationException($"Invalid command: {command}");
+}
+
+
+string DecodeString(string encodedValue)
+{
+    var firstIndex = encodedValue.IndexOf("i");
+    var lastIndex = encodedValue.IndexOf("e");
+    if (firstIndex == -1 || lastIndex == -1 || (firstIndex == -1 && lastIndex == -1))
+    {
+        throw new InvalidOperationException("Unhandled encoded value: " + encodedValue);
+    }
+    var strLength = lastIndex - firstIndex;
+    var numberString = encodedValue.Substring(firstIndex + 1, strLength - 1);
+    var isParsed = long.TryParse(numberString, out var parsedValue);
+    if (!isParsed)
+    {
+        throw new InvalidOperationException("Unhandled encoded value: " + encodedValue);
+    }
+    return JsonSerializer.Serialize(parsedValue);
+}
+string DecodeNumber(string encodedValue)
+{
+    var colonIndex = encodedValue.IndexOf(':');
+    if (colonIndex != -1)
+    {
+        var strLength = int.Parse(encodedValue[..colonIndex]);
+        var strValue = encodedValue.Substring(colonIndex + 1, strLength);
+        return JsonSerializer.Serialize(strValue);
+    }
+    else
+    {
+        throw new InvalidOperationException("Invalid encoded value: " + encodedValue);
+    }
 }
