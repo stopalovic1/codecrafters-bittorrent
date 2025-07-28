1ba3abfd0d0d00b90ed20f92176a635bea45dea7
Senad Topalovic <senad9898@gmail.com>
2025-07-28 16:40:55 +0200

    Fixed bencoding of list

---
+++ src/Bencoding.cs
@@ -0,0 +1,98 @@
+﻿using System.Text.Json;
+
+namespace codecrafters_bittorrent.src;
+
+public static class Bencoding
+{
+    private static List<object> _bencodingList = new();
+    public static long DecodeInteger(string encodedValue)
+    {
+        var firstIndex = encodedValue.IndexOf("i");
+        var lastIndex = encodedValue.IndexOf("e");
+        if (firstIndex == -1 || lastIndex == -1 || (firstIndex == -1 && lastIndex == -1))
+        {
+            throw new InvalidOperationException("Unhandled encoded value: " + encodedValue);
+        }
+        var strLength = lastIndex - firstIndex;
+        var numberString = encodedValue.Substring(firstIndex + 1, strLength - 1);
+        var isParsed = long.TryParse(numberString, out var parsedValue);
+        if (!isParsed)
+        {
+            throw new InvalidOperationException("Unhandled encoded value: " + encodedValue);
+        }
+
+        return parsedValue;
+    }
+    public static string DecodeString(string encodedValue)
+    {
+        var colonIndex = encodedValue.IndexOf(':');
+        if (colonIndex != -1)
+        {
+            var strLength = int.Parse(encodedValue[..colonIndex]);
+            var strValue = encodedValue.Substring(colonIndex + 1, strLength);
+            return strValue;
+        }
+        else
+        {
+            throw new InvalidOperationException("Invalid encoded value: " + encodedValue);
+        }
+    }
+    public static string DecodeList(string encodedValue)
+    {
+        if (string.IsNullOrEmpty(encodedValue))
+        {
+            var stack = new Stack<object>(_bencodingList);
+            var resultList = new List<object>();
+            string result = "";
+            while (stack.TryPop(out var element))
+            {
+                if (element is string || element is long)
+                {
+                    result = element + "," + result;
+                }
+                else
+                {
+                    result = result.Trim(',');
+                    if (result.Length > 0) 
+                    {
+                        var list = element as List<object>;
+                        list!.Add(result);
+                        result = "";
+                        resultList.Add(list);
+                    }     
+                }
+            }
+
+            return JsonSerializer.Serialize(resultList);
+        }
+
+        if (encodedValue[0] == 'l')
+        {
+            var firstIndex = encodedValue.IndexOf("l");
+            var lastIndex = encodedValue.LastIndexOf("e");
+            var strLength = lastIndex - firstIndex;
+            var listString = encodedValue.Substring(firstIndex + 1, strLength - 1);
+            _bencodingList.Add(new List<object>());
+            return DecodeList(listString);
+        }
+        else if (encodedValue[0] == 'i')
+        {
+            var decodedInteger = DecodeInteger(encodedValue);
+            var lastIndex = encodedValue.IndexOf("e");
+            var listString = encodedValue.Substring(lastIndex + 1);
+            _bencodingList.Add(decodedInteger);
+            return DecodeList(listString);
+        }
+        else if (Char.IsDigit(encodedValue[0]))
+        {
+            var decodedValue = DecodeString(encodedValue);
+            var decodedValueLength = decodedValue.Length;
+            var digitCount = (int)Math.Floor(Math.Log10(decodedValueLength) + 1);
+            var listString = encodedValue.Substring(decodedValueLength + digitCount + 1);
+            _bencodingList.Add(decodedValue);
+            return DecodeList(listString);
+        }
+
+        return DecodeList("");
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,4 @@
+using codecrafters_bittorrent.src;
 using System.Text.Json;
 
 // Parse arguments
@@ -8,27 +9,22 @@ var (command, param) = args.Length switch
     _ => (args[0], args[1])
 };
 
-// Parse command and act accordingly
 if (command == "decode")
 {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    //Console.WriteLine("Logs from your program will appear here!");
-
-    //Uncomment this line to pass the first stage
     var encodedValue = param;
     if (Char.IsDigit(encodedValue[0]))
     {
-        var decodedValue = DecodeString(encodedValue);
-        Console.WriteLine(decodedValue);
+        var decodedValue = Bencoding.DecodeString(encodedValue);
+        Console.WriteLine(JsonSerializer.Serialize(decodedValue));
     }
     else if (encodedValue[0] == 'i')
     {
-        var decodedValue = DecodeInteger(encodedValue);
-        Console.WriteLine(decodedValue);
+        var decodedValue = Bencoding.DecodeInteger(encodedValue);
+        Console.WriteLine(JsonSerializer.Serialize(decodedValue));
     }
     else if (encodedValue[0] == 'l')
     {
-        var decodedValue = DecodeList(encodedValue);
+        var decodedValue = Bencoding.DecodeList(encodedValue);
         Console.WriteLine(decodedValue);
     }
     else
@@ -43,61 +39,6 @@ else
 
 
 
-string DecodeInteger(string encodedValue)
-{
-    var firstIndex = encodedValue.IndexOf("i");
-    var lastIndex = encodedValue.IndexOf("e");
-    if (firstIndex == -1 || lastIndex == -1 || (firstIndex == -1 && lastIndex == -1))
-    {
-        throw new InvalidOperationException("Unhandled encoded value: " + encodedValue);
-    }
-    var strLength = lastIndex - firstIndex;
-    var numberString = encodedValue.Substring(firstIndex + 1, strLength - 1);
-    var isParsed = long.TryParse(numberString, out var parsedValue);
-    if (!isParsed)
-    {
-        throw new InvalidOperationException("Unhandled encoded value: " + encodedValue);
-    }
 
-    return JsonSerializer.Serialize(parsedValue);
-}
-string DecodeString(string encodedValue)
-{
-    var colonIndex = encodedValue.IndexOf(':');
-    if (colonIndex != -1)
-    {
-        var strLength = int.Parse(encodedValue[..colonIndex]);
-        var strValue = encodedValue.Substring(colonIndex + 1, strLength);
-        return JsonSerializer.Serialize(strValue);
-    }
-    else
-    {
-        throw new InvalidOperationException("Invalid encoded value: " + encodedValue);
-    }
-}
-string DecodeList(string encodedValue)
-{
-    var firstIndex = encodedValue.IndexOf("l");
-    var lastIndex = encodedValue.LastIndexOf("e");
-    if (firstIndex == -1 || lastIndex == -1 || (firstIndex == -1 && lastIndex == -1))
-    {
-        throw new InvalidOperationException("Unhandled encoded value: " + encodedValue);
-    }
-    var strLength = lastIndex - firstIndex;
-    var listString = encodedValue.Substring(firstIndex + 1, strLength - 1);
 
-    if (string.IsNullOrEmpty(listString))
-    {
-        return JsonSerializer.Serialize(Array.Empty<object>());
-    }
-    var splitArray = listString.Split(':');
-    var numberString = int.Parse(splitArray[0]);
-    var stringPart = splitArray[1].Substring(0, numberString);
-
-    var numberPart = splitArray[1].Substring(numberString);
-    var decodedNumberPart = DecodeInteger(numberPart);
-    object[] array = { stringPart, int.Parse(decodedNumberPart) };
-    string serialized = JsonSerializer.Serialize(array);
-    return serialized;
-}
 
