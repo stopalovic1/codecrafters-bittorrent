0bd77873b45ceac9636c74e39b57968558d53c3b
Senad Topalovic <senad9898@gmail.com>
2025-07-31 02:18:51 +0200

    changes

--- src/Program.cs
+++ src/Program.cs
@@ -2,16 +2,17 @@ using codecrafters_bittorrent.src;
 using System.Text.Json;
 
 // Parse arguments
-var (command, param) = args.Length switch
+var (command, param1, param2) = args.Length switch
 {
     0 => throw new InvalidOperationException("Usage: your_bittorrent.sh <command> <param>"),
     1 => throw new InvalidOperationException("Usage: your_bittorrent.sh <command> <param>"),
-    _ => (args[0], args[1])
+    3 => (args[0], args[1], args[2]),
+    _ => (args[0], args[1], null)
 };
 
 if (command == "decode")
 {
-    var encodedValue = param;
+    var encodedValue = param1;
     try
     {
         var decodedValue = Bencoding.Decode(encodedValue, 0);
@@ -24,26 +25,26 @@ if (command == "decode")
 }
 else if (command == "info")
 {
-    var path = param;
+    var path = param1;
     var result = await TorrentFileParser.ParseAsync(path);
     Console.WriteLine(result.ToString());
 }
 else if (command == "peers")
 {
-    var path = param;
+    var path = param1;
     var result = await TorrentPeersHandler.GetTorrentPeersAsync(path);
     var output = string.Join("\n", result);
     Console.WriteLine(output);
 }
 else if (command == "handshake")
 {
-    var path = param;
-    var hexString = await TorrentPeersHandler.InitiatePeerHandshakeAsync(path, param!);
+    var path = param1;
+    var hexString = await TorrentPeersHandler.InitiatePeerHandshakeAsync(path, param2!);
     Console.WriteLine($"Peer ID: {hexString}");
 }
 else if (command == "download_piece")
 {
-    var result = await TorrentPeersHandler.DownloadPieceAsync(param!, 0);
+    var result = await TorrentPeersHandler.DownloadPieceAsync(param1!, int.Parse(param2!));
     Console.WriteLine(result);
 }
 else
